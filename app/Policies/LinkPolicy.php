ff264f7bbac8a9cc8f6a2cea4593124ace9586d7
Sidrit Trandafili <forge405@gmail.com>
2023-06-30 08:48:14 +0200

    add LinkPolicy
    Signed-off-by: Sidrit Trandafili <forge405@gmail.com>

---
+++ app/Policies/LinkPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Link;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class LinkPolicy
+{
+    use HandlesAuthorization;
+
+    public function view(?User $user, Link $link): bool
+    {
+        $isTheLinkPublished = $link->publish_at <= now();
+
+        $isTheLinkAccepted = $link->status === 'accepted';
+
+        if($user === null) {
+            return $isTheLinkPublished && $isTheLinkAccepted;
+        }
+
+        $isTheLinkInDraftOrDeclinedAndTheUserIsTheAuthor = in_array($link->status, ['draft', 'declined']) && $user->id === $link->author_id;
+
+        return $isTheLinkPublished && ($isTheLinkAccepted || $isTheLinkInDraftOrDeclinedAndTheUserIsTheAuthor);
+    }
+
+    public function update(User $user, Link $link): bool
+    {
+        return $link->status === 'draft' && $user->id === $link->author_id;
+    }
+}
